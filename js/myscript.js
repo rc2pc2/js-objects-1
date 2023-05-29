08ad3118f90c632a2f7c977c0d62bc96b0e6594d
rc2pc2 <petriccarcc@gmail.com>
2023-05-29 12:25:40 +0200

    pepper snack example

--- js/myscript.js
+++ js/myscript.js
@@ -66,26 +66,25 @@
 //    console.log('----');
 // }
 
-const player = {
-   playerId : '',
-   firstName : 'Librona',
-   lastName: 'Giovanna',
-   age: 21,
-   appm : 0,
-   pct3 : 0,
-   blk:0,
-   shots: 0,
-}
+// const player = {
+//    playerId : '',
+//    firstName : 'Librona',
+//    lastName: 'Giovanna',
+//    age: 21,
+//    appm : 0,
+//    pct3 : 0,
+//    blk:0,
+//    shots: 0,
+// }
 
-player.appm = getRandomInt(0, 50);
-player.pct3 = getRandomInt(0, 100);
-player.blk = getRandomInt(0, 30);
-player.shots = getRandomInt(20, 100);
+// player.appm = getRandomInt(0, 50);
+// player.pct3 = getRandomInt(0, 100);
+// player.blk = getRandomInt(0, 30);
+// player.shots = getRandomInt(20, 100);
 
-player.playerId = generateRandomString(3, 'QWERTYUIOPASDFGHJKLZXCVBNM') + getRandomInt(0,9) + getRandomInt(0,9) + getRandomInt(0,9) ;
+// player.playerId = generateRandomString(3, 'QWERTYUIOPASDFGHJKLZXCVBNM') + getRandomInt(0,9) + getRandomInt(0,9) + getRandomInt(0,9);
 
-console.log(player);
-
+// console.log(player);
 
 /**
  * Function that generates a random number (not secure) between two values, both included.
@@ -121,3 +120,36 @@ function generateRandomString(length, allowedChars ) {
 
    return result;
 }
+
+const types = ['red bell', 'green bell', 'yellow bell', 'abenhero', 'jalapeno', 'calabrese' ];
+
+
+function generatePeppers(quantity, typesList ){
+
+   const generatedPeppers = [];
+
+   for (let index = 0; index < quantity; index++) {
+      const newPepper = {
+         type: typesList[ getRandomInt(0, typesList.length - 1) ],
+         weight: getRandomInt(0, 700),
+         length: getRandomInt(0, 100),
+      };
+
+      generatedPeppers.push(newPepper);
+   }
+
+   return generatedPeppers;
+}
+
+
+const peppers = generatePeppers(10, types);
+
+console.log(peppers);
+
+let sumOfWeights = 0;
+
+for (let index = 0; index < peppers.length; index++) {
+   sumOfWeights += peppers[index].weight;
+}
+
+console.log (sumOfWeights);
