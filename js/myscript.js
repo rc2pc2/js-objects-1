11d83ac2de049d27e992e63733b5ea4f526a4fa9
rc2pc2 <petriccarcc@gmail.com>
2023-05-29 10:33:33 +0200

    array of objects example

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,61 @@
-const studentessa = {
-   nome: 'Ginetta',
-   cognome: 'Gini',
-   'anno-di-nascita' : 1990,
-   "corso" : 'Informatica',
+// const studentessa = {
+//    nome: 'Ginetta',
+//    cognome: 'Gini',
+//    'anno-di-nascita' : 1990,
+//    "corso" : 'Informatica',
+// };
+
+// // ? per ogni chiave chiamata chiave esistente nell'oggetto letterale studentessa
+// for (const chiave in studentessa) {
+//    console.log(chiave, studentessa[chiave]);
+// }
+
+const studenti = [
+   {
+      nome: 'Ginetta',
+      cognome: 'Gini',
+      'anno-di-nascita' : 1990,
+      "corso" : 'Informatica',
+   },
+
+   {
+      nome: 'Gino',
+      cognome: 'Rossi',
+      'anno-di-nascita' : 1995,
+      "corso" : 'Matemagica',
+   },
+
+   {
+      nome: 'Genoveffina',
+      cognome: 'Lorenzetti',
+      'anno-di-nascita' : 1988,
+      "corso" : 'Matemagica',
+   },
+
+   {
+      nome: 'Gingirello',
+      cognome: 'Del Pero',
+      'anno-di-nascita' : 1977,
+      "corso" : 'Filosofia',
+   }
+];
+
+const nuovaStudentessa = {
+   nome: 'Mimma',
+   cognome: 'Ninni',
+   'anno-di-nascita':  1966,
+   corso: 'Psicopedalogia'
 };
 
-// ? per ogni chiave chiamata chiave esistente nell'oggetto letterale studentessa
-for (const chiave in studentessa) {
-   console.log(chiave, studentessa[chiave]);
+studenti.push(nuovaStudentessa);
+
+for (let index = 0; index < studenti.length; index++) {
+   const studente = studenti[index];
+
+   for (chiave in studente) {
+      console.log(chiave, ':', studente[chiave]);
+   }
+
+   console.log('----')
+
 }
